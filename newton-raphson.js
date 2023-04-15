c9f04546c2360644d2a1091551a22c31062080ad
Babatunde Koiki <koikibabatunde14@gmail.com>
2023-04-15 16:32:31 +0100

    Newton-Raphson method

---
+++ newton-raphson.js
@@ -0,0 +1,23 @@
+/* Define the function we want to find the roots of */
+const f = (x) => Math.pow(x, 3) - 2 * Math.pow(x, 2) + 1;
+
+/* Define the derivative of the function */
+const df = (x) => 3 * Math.pow(x, 2) - 4 * x;
+
+/* Define the Newton-Raphson method function */
+function newtonRaphson(x0, tol) {
+  let xn = x0;  // Initial guess
+  let fxn = f(xn);  // Function value at initial guess
+
+  // Iterate until the approximation converges to desired tolerance level
+  while (Math.abs(fxn) > tol) {
+    let dfxn = df(xn);  // Derivative value at current approximation
+    xn = xn - fxn / dfxn;  // Apply Newton-Raphson formula
+    fxn = f(xn);  // Function value at new approximation
+  }
+
+  return xn;  // Return the final approximation of the root
+}
+
+// Test the Newton-Raphson method with an initial guess of 1 and tolerance of 0.0001
+console.log("Root approximation: ", newtonRaphson(1, 0.0001));
